b7e5a368c802aa3eb2386e6790c91f5700893f4b
Hamdan Asim <hamdanasim@Hamdans-MacBook-Pro.local>
2022-08-22 15:22:07 +0500

    Schema for whatsapp Chat
    WHAT: Database model for whatsapp chat
    
    WHY: because it was our task
    
    IMPACT: HIGH

--- mysite/anotherPoll/models.py
+++ mysite/anotherPoll/models.py
@@ -1,3 +1,37 @@
+from email import message
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_id = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    profile_pic = models.ImageField()
+    is_active = models.BooleanField(default=False)
+    last_seen = models.DateTimeField("last seen date time")
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent_msg = models.ForeignKey()
+    msg_id = models.CharField(max_length=20)
+    msg_body = models.CharField(max_length=300)
+    create_date = models.DateTimeField("Message create date time")
+
+
+class Group(models.Model):
+    members = models.ManyToManyField(User)
+    group_id = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    date_created = models.DateTimeField("Date group was created")
+    is_active = models.BooleanField(default=False)
+
+
+class message_recipient(models.Model):
+    recipient = models.OneToOneField(User)
+    message = models.OneToOneField(Message)
+    is_read = models.BooleanField(default=False)
+    is_delivered = models.BooleanField(default=False)
